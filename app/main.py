789f5bdde66169d5dcfd624de863ccb63670af7c
Chiyembekezo Chilembwe <phoenixtech2003@gmail.com>
2025-06-26 11:23:08 +0200

    Add CORS  config

--- app/main.py
+++ app/main.py
@@ -1,9 +1,21 @@
 from fastapi import FastAPI
 from .routers import inference
 from dotenv import load_dotenv
+from fastapi.middleware.cors import CORSMiddleware
 
 load_dotenv()
 
 app = FastAPI()
+origins = [
+    "http://localhost:300"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_methods=["POST"],
+    allow_headers=["*"]
+)
+
 app.include_router(inference.router)
 
